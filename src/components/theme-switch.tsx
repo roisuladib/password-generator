77f739911250088595a4392e0ff0d92e88813683
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2025-04-15 16:13:35 +0700

    refactor

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -15,106 +15,104 @@ import useSound from 'use-sound';
 import { MoonFilledIcon, SunFilledIcon } from '^/components';
 
 export interface ThemeSwitchProps {
-   className?: string;
-   classNames?: SwitchProps['classNames'];
+  className?: string;
+  classNames?: SwitchProps['classNames'];
 }
 
 export const ThemeSwitch: FC<ThemeSwitchProps> = ({ className, classNames }) => {
-   const { theme, setTheme } = useTheme();
-   const isSSR = useIsSSR();
-   const ref = useRef<HTMLDivElement>(null);
-   const [playLight] = useSound('/sounds/switch-on.mp3');
-   const [playNight] = useSound('/sounds/switch-off.mp3');
+  const { theme, setTheme } = useTheme();
+  const isSSR = useIsSSR();
+  const ref = useRef<HTMLDivElement>(null);
+  const [playLight] = useSound('/sounds/switch-on.mp3');
+  const [playNight] = useSound('/sounds/switch-off.mp3');
 
-   const handleSetTheme = useCallback(() => {
-      if (theme === 'light') {
-         playNight();
-         setTheme('dark');
-      } else {
-         playLight();
-         setTheme('light');
-      }
-   }, [playLight, playNight, setTheme, theme]);
+  const isLight = theme === 'light';
+  const nextTheme = isLight ? 'dark' : 'light';
 
-   const onChange = useCallback(async () => {
-      const isAppearanceTransition =
-         typeof document !== 'undefined' &&
-         !!document.startViewTransition &&
-         !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+  const handleSetTheme = useCallback(() => {
+    isLight ? playNight() : playLight();
+    setTheme(nextTheme);
+  }, [isLight, nextTheme, playLight, playNight, setTheme]);
 
-      if (!isAppearanceTransition || !ref.current) {
-         handleSetTheme();
-         return;
-      }
+  const onChange = useCallback(async () => {
+    const isAppearanceTransition =
+      typeof document !== 'undefined' &&
+      !!document.startViewTransition &&
+      !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
-      const { top, left, width, height } = ref.current.getBoundingClientRect();
-      const x = left + width / 2;
-      const y = top + height / 2;
-      const right = window.innerWidth - left;
-      const bottom = window.innerHeight - top;
-      const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom));
+    if (!isAppearanceTransition || !ref.current) {
+      handleSetTheme();
+      return;
+    }
 
-      await document.startViewTransition(() => {
-         flushSync(() => {
-            handleSetTheme();
-         });
-      }).ready;
+    const { top, left, width, height } = ref.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
+    const right = window.innerWidth - left;
+    const bottom = window.innerHeight - top;
+    const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom));
 
-      const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${maxRadius}px at ${x}px ${y}px)`];
-      document.documentElement.animate(
-         {
-            clipPath: theme === 'light' ? [...clipPath].reverse() : clipPath,
-         },
-         {
-            duration: 500,
-            easing: 'ease-in',
-            pseudoElement:
-               theme === 'light' ? '::view-transition-old(root)' : '::view-transition-new(root)',
-         },
-      );
-   }, [handleSetTheme, theme]);
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        handleSetTheme();
+      });
+    }).ready;
 
-   const { Component, slots, isSelected, getBaseProps, getInputProps, getWrapperProps } = useSwitch(
+    const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${maxRadius}px at ${x}px ${y}px)`];
+    document.documentElement.animate(
       {
-         'isSelected': theme === 'light' || isSSR,
-         'aria-label': `Switch to ${theme === 'light' || isSSR ? 'dark' : 'light'} mode`,
-         onChange,
+        clipPath: theme === 'light' ? [...clipPath].reverse() : clipPath,
       },
-   );
+      {
+        duration: 500,
+        easing: 'ease-in',
+        pseudoElement:
+          theme === 'light' ? '::view-transition-old(root)' : '::view-transition-new(root)',
+      },
+    );
+  }, [handleSetTheme, theme]);
 
-   return (
-      <Component
-         {...getBaseProps({
-            className: cn(
-               'px-px transition-opacity hover:opacity-80 cursor-pointer',
-               className,
-               classNames?.base,
-            ),
-         })}>
-         <VisuallyHidden>
-            <input {...getInputProps()} />
-         </VisuallyHidden>
-         <div
-            {...getWrapperProps()}
-            ref={ref}
-            className={slots.wrapper({
-               class: cn(
-                  [
-                     'h-auto w-auto',
-                     'bg-transparent',
-                     'rounded-lg',
-                     'flex items-center justify-center',
-                     'group-data-[selected=true]:bg-transparent',
-                     '!text-default-500',
-                     'pt-px',
-                     'px-0',
-                     'mx-0',
-                  ],
-                  classNames?.wrapper,
-               ),
-            })}>
-            {!isSelected || isSSR ? <SunFilledIcon size={22} /> : <MoonFilledIcon size={22} />}
-         </div>
-      </Component>
-   );
+  const { Component, slots, isSelected, getBaseProps, getInputProps, getWrapperProps } = useSwitch({
+    isSelected: theme === 'light' || isSSR,
+    'aria-label': `Switch to ${nextTheme} mode`,
+    onChange,
+  });
+
+  return (
+    <Component
+      {...getBaseProps({
+        className: cn(
+          'px-px transition-opacity hover:opacity-80 cursor-pointer',
+          className,
+          classNames?.base,
+        ),
+      })}
+    >
+      <VisuallyHidden>
+        <input {...getInputProps()} />
+      </VisuallyHidden>
+      <div
+        {...getWrapperProps()}
+        ref={ref}
+        className={slots.wrapper({
+          class: cn(
+            [
+              'h-auto w-auto',
+              'bg-transparent',
+              'rounded-lg',
+              'flex items-center justify-center',
+              'group-data-[selected=true]:bg-transparent',
+              '!text-default-500',
+              'pt-px',
+              'px-0',
+              'mx-0',
+            ],
+            classNames?.wrapper,
+          ),
+        })}
+      >
+        {!isSelected || isSSR ? <SunFilledIcon size={22} /> : <MoonFilledIcon size={22} />}
+      </div>
+    </Component>
+  );
 };
